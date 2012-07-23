22ff95dd715c0c56c8e8f72e354d8279b7dee492
Godefroid Chapelle <gotcha@bubblenet.be>
2012-07-23 18:09:57 +0200

    take care of subscribers

--- cleanup_zope_persistent_registry/__init__.py
+++ cleanup_zope_persistent_registry/__init__.py
@@ -1,17 +1,31 @@
 def cleanup(registry):
-    ad = registry._adapters
-    for i in range(len(ad)):
-        cleanup_empty_adapters(ad[i])
+    adapters = registry._adapters
+    for i in range(len(adapters)):
+        cleanup_empty_adapters(adapters[i])
+    subscribers = registry._subscribers
+    for i in range(len(subscribers)):
+        cleanup_empty_subscribers(subscribers[i])
+    registry.p_changed = True
 
 
 def cleanup_empty_adapters(map):
     keys = map.keys()
     for key in keys:
         sub = map[key]
-        #if type(sub) == type((,)):
-        #    if not sub:
-        #        del map[key]
-        if type(sub) == type({}):
+        if type(sub) == dict:
             cleanup_empty_adapters(sub)
+            if not sub:
+                del map[key]
+
+
+def cleanup_empty_subscribers(map):
+    keys = map.keys()
+    for key in keys:
+        sub = map[key]
+        if type(sub) == tuple:
+            if not sub:
+                del map[key]
+        if type(sub) == dict:
+            cleanup_empty_subscribers(sub)
             if not sub:
                 del map[key]
